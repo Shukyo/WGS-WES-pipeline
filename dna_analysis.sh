81d44c92c4544a73dd488beb33917c7a7cdd460c
Shukyo <shuyang1986@gmail.com>
2018-12-03 17:41:46 +0800

    Define the functions

--- dna_analysis.sh
+++ dna_analysis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 dir=`dirname $0`
-source $dir/dna_analysis.part0 # defined the functions
-source $dir/dna_analysis.part1 # defined the default parameters
-source $dir/dna_analysis.part2 # defined the help document
-source $dir/dna_analysis.part3 # recieve the external parameters
-source $dir/dna_analysis.part4 # check the refer/input files
-source $dir/dna_analysis.part5 # output the basic information for the run
-source $dir/dna_analysis.part6 # main programe
+source $dir/dna_analysis.defFun # defined the functions
+source $dir/dna_analysis.defRef # defined the default parameters
+source $dir/dna_analysis.help # defined the help document
+source $dir/dna_analysis.defInput # recieve the external parameters
+source $dir/dna_analysis.check # check the refer/input files
+source $dir/dna_analysis.printHead # output the basic information for the run
+source $dir/dna_analysis.run # main programe

e3c87388d514a92300abc3325d3a821018aa96e9
Shukyo <shuyang1986@gmail.com>
2018-12-03 21:04:27 +0800

    amend

--- dna_analysis.sh
+++ dna_analysis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 dir=`dirname $0`
-source $dir/dna_analysis.defFun # defined the functions
-source $dir/dna_analysis.defRef # defined the default parameters
-source $dir/dna_analysis.help # defined the help document
-source $dir/dna_analysis.defInput # recieve the external parameters
-source $dir/dna_analysis.check # check the refer/input files
-source $dir/dna_analysis.printHead # output the basic information for the run
-source $dir/dna_analysis.run # main programe
+source $dir/dna_analysis/dna_analysis.defFun # defined the functions
+source $dir/dna_analysis/dna_analysis.defRef # defined the default parameters
+source $dir/dna_analysis/dna_analysis.help # defined the help document
+source $dir/dna_analysis/dna_analysis.defInput # recieve the external parameters
+source $dir/dna_analysis/dna_analysis.check # check the refer/input files
+source $dir/dna_analysis/dna_analysis.printHead # output the basic information for the run
+source $dir/dna_analysis/dna_analysis.run # main programe
